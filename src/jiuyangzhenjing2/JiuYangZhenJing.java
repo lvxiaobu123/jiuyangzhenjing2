6e8e7c0b432dcd6c4eb99cf8699567d67baa59aa
lvtou_global <771519523@qq.com>
2017-07-26 19:47:53 +0800

    first commit

---
+++ src/jiuyangzhenjing2/JiuYangZhenJing.java
@@ -0,0 +1,14 @@
+/**
+ * LT
+ */
+package jiuyangzhenjing2;
+
+/**
+ * @author LT
+ * 2017年7月26日
+ */
+public class JiuYangZhenJing {
+	public static void main(String[] args) {
+		System.out.println("aaaaaaaa");
+	}
+}
